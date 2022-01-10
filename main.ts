e14db81c189cc5f93d3f46e6f028e6cbbd4af8a0
cstnn <35491404+cstnn@users.noreply.github.com>
2022-01-10 20:14:51 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . . .
+        . # . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # . # .
+        . # # . .
+        . # # . .
+        . # . # .
+        . # . . #
+        `)
+})
